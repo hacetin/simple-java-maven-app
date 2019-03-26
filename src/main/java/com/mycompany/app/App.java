467b5d3cea71a17e92b3dcaf96762a9693d0c151
unknown <hamdialperencetin@gmail.com>
2019-03-26 22:28:05 +0300

    commit for jenkins

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -13,6 +13,7 @@ public class App
 
     public static void main(String[] args) {
         System.out.println(new App().getMessage());
+        System.out.println("jenkins");
     }
 
     private final String getMessage() {
