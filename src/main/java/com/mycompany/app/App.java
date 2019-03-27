ef7a7797f02c6a057bc012f4af77c0018b39e36d
Alperen Çetin <hamdialperencetin@gmail.com>
2019-03-27 20:40:44 +0300

    Update App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -14,6 +14,7 @@ public class App
     public static void main(String[] args) {
         System.out.println(new App().getMessage());
         System.out.println("jenkins");
+        System.out.println("jenkins2");
     }
 
     private final String getMessage() {
