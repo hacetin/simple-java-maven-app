21efcbf57c9ab32cb3d9f22719a1df92669e4951
unknown <hamdialperencetin@gmail.com>
2019-03-30 12:54:01 +0300

    return to first commit

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -1,24 +1,21 @@
-package com.mycony.app;
+package com.mycompany.app;
 
 /**
  * Hello world!
- * First commit
  */
-public cla
+public class App
 {
 
     private final String message = "Hello World!";
-    int a = 5;
+
     public App() {}
 
     public static void main(String[] args) {
         System.out.println(new App().getMessage());
-        System.out.println("jenkins");
-        System.out.println("jenkin
-    
-
-    private final String getMessage() {
-        return mess
     }
 
+    private final String getMessage() {
+        return message;
+    }
 
+}
